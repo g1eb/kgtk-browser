d871c063592a30631e23569edc45b55d84a7e135
g1eb <mail@g1eb.com>
2021-10-13 09:06:46 -0700

    Add indicators of which script is currently running in post deploy

--- post_deploy.sh
+++ post_deploy.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 set -e
 
+echo -e "\n >>> Running: 01-PREPARE_DATA_FOR_BROWSER.sh \n"
+
 ./venice/01-PREPARE_DATA_FOR_BROWSER.sh
+
+echo -e "\n >>> Running: 02-BUILD_THE_GRAPH_CACHE.sh \n"
+
 ./venice/02-BUILD_THE_GRAPH_CACHE.sh
+
+echo -e "\n >>> all done, timestamp: \n"
+
+echo `date -u`
