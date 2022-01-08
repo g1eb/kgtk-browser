d57eaa57bf7acb6d95511fe12a2d474eafc4bdd9
Gleb Satyukov <gleb@minds03.isi.edu>
2022-01-07 20:09:45 -0800

    Merge commit

--- post_deploy.sh
+++ post_deploy.sh
@@ -14,7 +14,7 @@ echo -e "\n >>> Running: 02-BUILD_THE_GRAPH_CACHE.sh \n"
 
 echo -e "\n >>> Running: 03-PREPARE_PAGERANK.sh \n"
 
-./venice/03-PREPARE_PAGERANK.py
+./venice/03-PREPARE_PAGERANK.sh
 
 
 echo -e "\n >>> all done, timestamp: \n"
