6d5c24abf02430c615101d309733f7d8bbcb9e6d
g1eb <mail@g1eb.com>
2023-03-03 10:53:56 -0800

    Update venice database generate script

---
+++ venice/generate.py
@@ -0,0 +1,77 @@
+import os
+import pathlib
+import papermill as pm
+from datetime import datetime
+from kgtk.functions import kgtk, kypher
+from kgtk.configure_kgtk_notebooks import ConfigureKGTK
+
+
+# Get current directory
+current_dir = os.getcwd()
+
+
+# Create temporary directory
+temp_dir = os.path.join(current_dir, 'temp')
+os.makedirs(temp_dir, exist_ok=True)
+
+
+# Create output directory
+date = datetime.now().strftime('%Y_%m_%d')
+output_dir = os.path.join(current_dir, 'output/{}'.format(date))
+os.makedirs(output_dir, exist_ok=True)
+
+
+# Configure KGTK
+ck = ConfigureKGTK(['all'])
+ck.configure_kgtk(
+    input_graph_path=current_dir,
+    output_path=output_dir,
+    project_name='venice',
+)
+
+
+# Execute script 01-partition to partition the data
+pm.execute_notebook(
+    '{}/{}'.format(current_dir, 'utils/01-partition.ipynb'),
+    '{}/{}'.format(temp_dir, '01-partition.output.ipynb'),
+    parameters=dict(
+        wikidata_input_path='{}/{}'.format(current_dir, 'input.tsv'),
+        wikidata_parts_path=temp_dir,
+        temp_folder_path=temp_dir,
+        gzip_command='gzip',
+        use_mgzip='False',
+    )
+)
+
+
+# Calculate pagerank
+kgtk('''
+  --debug graph-statistics
+  -i {}/claims.tsv.gz
+  -o {}/metadata.pagerank.undirected.tsv.gz
+  --compute-pagerank True
+  --compute-hits False
+  --page-rank-property Pundirected_pagerank
+  --output-degrees False
+  --output-pagerank True
+  --output-hits False
+  --output-statistics-only
+  --undirected True
+  --log-file {}/metadata.pagerank.undirected.summary.txt
+'''.format(*[temp_dir]*3))
+
+
+# Execute script 02-create-db to create the sqlite3.db file
+pm.execute_notebook(
+    '{}/{}'.format(current_dir, 'utils/02-create-db.ipynb'),
+    '{}/{}'.format(temp_dir, '02-create-db.output.ipynb'),
+    kernel_name='python3',
+    parameters=dict(
+        input_path=temp_dir,
+        output_path=output_dir,
+        project_name='venice',
+        create_class_viz='no',
+        create_db='yes',
+        create_es='no',
+    )
+)
